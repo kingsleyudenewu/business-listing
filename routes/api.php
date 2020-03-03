699f07d2c70e4611cce7e2c928baa21046317c43
Kingsley Udenewu <kingzpacking@gmail.com>
2020-03-03 14:12:49 +0100

    Add search api

--- routes/api.php
+++ routes/api.php
@@ -17,4 +17,4 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('/search', 'BusinessListingController@searchListing');
+Route::get('/search_api', 'BusinessListingController@searchListing');
